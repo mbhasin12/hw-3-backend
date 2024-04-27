ac48bc9c980ad767b221459025b67f6c29783a16
mbhasin1 <mbhasin@purdue.edu>
2024-04-27 12:09:31 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,96 @@
+from flask import Flask, request, jsonify
+import boto3
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from flask_cors import CORS
+from io import StringIO
+import numpy as np
+
+app = Flask(__name__)
+CORS(app)
+s3_client = boto3.client('s3')
+
+@app.route('/process-csv', methods=['POST'])
+def process_csv():
+    print('yo')
+    request_data = request.json
+
+    key = request_data['filename']
+    bucket_name = 'kmeans-bucket-mbhasin'
+
+    # Get the CSV file from S3
+    response = s3_client.get_object(Bucket=bucket_name, Key=key)
+    content = response['Body'].read().decode('utf-8')
+
+    # Read the CSV into a pandas DataFrame
+    df = pd.read_csv(StringIO(content))
+    df = df.dropna()
+    if len(df.columns) > 2:
+        # Normalize the data
+        scaler = StandardScaler()
+        df_normalized = scaler.fit_transform(df)
+
+        # PCA only if more than 2 columns
+        pca = PCA(n_components=2)
+        principalComponents = pca.fit_transform(df_normalized)
+
+        print(principalComponents)
+
+        # Elbow method to find the optimal number of clusters
+        sum_of_squared_distances = []
+        K = range(1, 15)
+        for k in K:
+            km = KMeans(n_clusters=k)
+            km = km.fit(principalComponents)
+            sum_of_squared_distances.append(km.inertia_)
+        optimal_k = sum_of_squared_distances.index(min(sum_of_squared_distances)) + 1
+
+        # K-Means with optimal_k
+        #optimal_k = 4
+        kmeans = KMeans(n_clusters=optimal_k)
+        df['cluster'] = kmeans.fit_predict(principalComponents)
+
+        pca_df = pd.DataFrame(principalComponents, columns=['PCA1', 'PCA2'])
+        pca_df['Cluster'] = df['cluster'].to_numpy()
+
+        print(pca_df.head(5))
+
+        # Convert the clustered data to JSON
+        result_json = pca_df.to_json(orient='records')
+
+        # Save the JSON to S3
+        result_key = f'results/{key}-result.json'
+        s3_client.put_object(Body=result_json, Bucket=bucket_name, Key=result_key)
+
+        return result_json
+    else:
+        # Elbow method to find the optimal number of clusters
+        sum_of_squared_distances = []
+        K = range(1, 15)
+        for k in K:
+            km = KMeans(n_clusters=k)
+            km = km.fit(df)
+            sum_of_squared_distances.append(km.inertia_)
+        optimal_k = sum_of_squared_distances.index(min(sum_of_squared_distances)) + 1
+
+        # K-Means with optimal_k
+        #optimal_k = 4
+        kmeans = KMeans(n_clusters=optimal_k)
+        df['Cluster'] = kmeans.fit_predict(df)
+
+        df.rename(columns={col: f'PCA{idx + 1}' for idx, col in enumerate(df.columns[:2])}, inplace=True)
+
+        # Convert the clustered data to JSON
+        result_json = df.to_json(orient='records')
+
+        # Save the JSON to S3
+        result_key = f'results/{key}-result.json'
+        s3_client.put_object(Body=result_json, Bucket=bucket_name, Key=result_key)
+
+        return result_json
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
